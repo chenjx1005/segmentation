98bb0d8146c9088b3404095c3b5ef4f6ee4d15df
Chen junxuan <chenjx1005@gmail.com>
2014-03-05 11:23:41 +0800

    optical flow

---
+++ cv_segment/PottsModel.hpp
@@ -0,0 +1,79 @@
+#ifndef SEGMENTATION_POTTSMODEL_H
+#define SEGMENTATION_POTTSMODEL_H
+
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <list>
+
+#include "opencv2\opencv.hpp"
+
+class PottsModel
+{
+public:
+	typedef cv::Vec<double, 8> Vec8d;
+	typedef cv::Matx<int, 3, 3> Matx33i;
+	enum ColorSpace {HSV = 1, RGB = 2,};
+	//color is a hsv/bgr Mat(CV_8UC3), depth is a gray Mat(CV_8U)
+    PottsModel(const cv::Mat &color, const cv::Mat &depth, int color_space=HSV);
+    PottsModel(const cv::Mat &color, int color_space=HSV);
+	virtual ~PottsModel();
+	void ComputeDifference();
+	double PixelEnergy(int pi, int pj) const;
+	void MetropolisOnce();
+	bool iterable() const { return t_ >= min_t_; }
+	void GenStatesResult();
+	void ShowStates(int milliseconds=0);
+	void SaveStates();
+	//update states of the model after label
+	void UpdateStates(const std::vector<std::vector<int> > &states);
+	void GenBoundry();
+	void ShowBoundry(int milliseconds=0) const
+	{
+		cv::imshow("PottsModel", boundry_);
+		cv::waitKey(milliseconds);
+	}
+	void SaveBoundry() const { cv::imwrite("boundry.jpg", boundry_); }
+	cv::Mat get_boundrymap() const { return boundry_; }
+	void set_temperature(double t) { t_ = t; }
+	void Freeze() { t_ = min_t_; }
+	void ShowDifference() const;
+	void HorizontalColor() const;
+	void VerticalColor() const;
+	void RightDiagColor() const;
+	void LeftDiagColor() const;
+	double Distance(const cv::Vec3b &a, const cv::Vec3b &b) const;
+	
+private:
+	//the factor for computing the averaged color vector difference of all
+	//neighbors<i, j> and range in [0, 10]
+	double alpha_;
+	//default 256 for convenience
+	int num_spin_;
+	double init_t_;
+	double min_t_;
+	//the annealing coefficient, less than 1
+	double a_c_;
+	double t_;
+	//the Boltzman constant
+	const double kK;
+	//the neighbors of the computed pixel, {{4, 2, 6}, {0, -1, 1}, {7, 3 ,5}}
+	//4, 2, 6
+	//0,-1, 1
+	//7, 3, 5
+	const Matx33i kPixel;
+	//the J for the difference of depthes of pixels > 30cm
+	const int kMaxJ;
+	double mean_diff_;
+	int num_result_;
+	int num_result_gen_;
+	cv::Mat color_;
+	cv::Mat depth_;
+	cv::Mat diff_;
+	cv::Mat states_result_;
+	cv::Mat boundry_;
+	std::vector<std::vector<int> > states_;
+	int color_space_;
+};
+#endif
+
---
+++ cv_segment/cvtest.cpp
@@ -0,0 +1,67 @@
+#include "PottsModel.hpp"
+#include "FastLabel.hpp"
+#include "OpticalFlow.cpp"
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	optical();
+	Mat img;
+	img = imread("Color0.png");
+	//cvtColor(imread("Color0.png"), img, CV_BGR2HSV);
+	Mat depth;
+	cvtColor(imread("Depth0.png"), depth, CV_BGR2GRAY);
+	
+	//PottsModel potts_model(img);
+	PottsModel potts_model(img, PottsModel::RGB);
+	//potts_model.ShowDifference();
+	while (potts_model.iterable()){
+		potts_model.MetropolisOnce();
+		potts_model.SaveStates();
+	}
+	potts_model.GenBoundry();
+	potts_model.ShowBoundry(5000);
+	potts_model.SaveBoundry();
+	Mat boundry = potts_model.get_boundrymap();
+	//Mat boundry = imread("b.jpg", 0);
+	FastLabel f(boundry);
+	f.FirstScan();
+	potts_model.UpdateStates(f.get_labels());
+	potts_model.SaveStates();
+	f.SecondScan();
+	potts_model.UpdateStates(f.get_labels());
+	potts_model.SaveStates();
+	for (int j=0; j < 4; j++) {
+		potts_model.MetropolisOnce();
+		potts_model.SaveStates();
+	}
+	potts_model.GenBoundry();
+	potts_model.SaveBoundry();
+	boundry = potts_model.get_boundrymap();
+	//Mat boundry = imread("b.jpg", 0);
+	FastLabel f2(boundry);
+	f2.FirstScan();
+	potts_model.UpdateStates(f2.get_labels());
+	potts_model.SaveStates();
+	f2.SecondScan();
+	potts_model.UpdateStates(f2.get_labels());
+	potts_model.SaveStates();
+	/*for (int i=0; i < 3; i++)
+	{
+		potts_model.Freeze();
+		for (int j=0; j < 4; j++) {
+			potts_model.MetropolisOnce();
+			potts_model.SaveStates();
+		}
+		potts_model.GenBoundry();
+		potts_model.SaveBoundry();
+		FastLabel f2(potts_model.get_boundrymap());
+		f2.FirstScan();
+		f2.SecondScan();
+		potts_model.UpdateStates(f2.get_labels());
+		potts_model.SaveStates();
+	}*/
+    return 0;
+}
